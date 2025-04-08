7b86eac86770e5c5dcf3881d8c76659a15f845a7
Hoi Lau <lauhoi2010@gmail.com>
2025-04-09 01:28:31 +0700

    feat: setup project

---
+++ src/main/java/com/nashrookie/lavish/LavishApplication.java
@@ -0,0 +1,13 @@
+package com.nashrookie.lavish;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LavishApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LavishApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/nashrookie/lavish/LavishApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nashrookie.lavish;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LavishApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
